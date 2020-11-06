9c670ddbf3ca787392a3fca74695b6af07d1e515
Maya Salcedo <68334235+maya-salcedo@users.noreply.github.com>
2020-11-06 13:43:24 +0200

    Add files via upload
    fixed functions and styling

--- main.py
+++ main.py
@@ -12,21 +12,25 @@ class Screen1(Screen):
 
     def search_definition(self, word):
         if word in data:
-            self.ids.definition_list.text = str(data[word]).replace("['", "").replace("']", "").replace("', '", "\n")
+            self.ids.definition_list.text = str(data[word]).replace("['", "-").replace("']", "").replace("', '", "\n-")
         elif word.lower() in data:
-            self.ids.definition_list.text = str(data[word.lower()]).replace("['", "").replace("']", "").replace("', '", "\n")
+            self.ids.definition_list.text = str(data[word.lower()]).replace("['", "-").replace("']", "").replace("', '", "\n-")
         elif word.title() in data:
-            self.ids.definition_list.text = str(data[word.title()]).replace("['", "").replace("']", "").replace("', '", "\n")
+            self.ids.definition_list.text = str(data[word.title()]).replace("['", "-").replace("']", "").replace("', '", "\n-")
         elif word.upper() in data:
-            self.ids.definition_list.text = str(data[word.upper()]).replace("['", "").replace("']", "").replace("', '", "\n")
+            self.ids.definition_list.text = str(data[word.upper()]).replace("['", "-").replace("']", "").replace("', '", "\n-")
         else:
             try:
                 alternative_word = [difflib.get_close_matches(word, data, n=1)]
                 word_alternate = str(alternative_word[0]).replace("['", "").replace("']", "")
-                data[word_alternate].insert(0, "Do you mean " + word_alternate + "?")
-                self.ids.definition_list.text = str(data[word_alternate]).replace("['", "").replace("']", "").replace("', '", "\n")
+                list = data[word_alternate] #creates new list to avoid writing to the database
+                list.insert(0, "Did you mean " + word_alternate + "?")
+                self.ids.definition_list.text = str(data[word_alternate]).replace("['", " ").replace("']", "").replace("', '", "\n-")
+                print(self.ids.definition_list.text)
+                print(type(self.ids.definition_list.text))
             except KeyError:
-                self.ids.definition_list.text = "Word is not available in this dictionary. Check your spelling."
+                self.ids.definition_list.text = "That word is not available in this dictionary. Check your spelling."
+
 
     def clear(self, not_word):
         self.ids.word_to_look.text = " "
@@ -36,9 +40,9 @@ class Screen1(Screen):
 class RootWidget(ScreenManager):
     pass
 
-class MainApp(App):
+class DictionaryApp(App):
     def build(self):
         return RootWidget() #obj, not class
 
 if __name__ == "__main__":
-    MainApp().run()
+    DictionaryApp().run()
